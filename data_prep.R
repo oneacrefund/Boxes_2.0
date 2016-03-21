0734ae10a28cbcd1ed0c46820c05c5894105db06
Colin Custer <colin.custer@oneacrefund.org>
2016-03-21 20:08:46 +0300

    Initial commit - data prep script

---
+++ data_prep.R
@@ -0,0 +1,5 @@
+# Boxes 2.0: Data prep script
+# Author: Colin Custer (colin.custer@oneacrefund.org)
+# Description: A script to prep data for upload and storage on the Shiny server
+# Date created: 21 Mar 2016
+# Date modified: 21 Mar 2016
