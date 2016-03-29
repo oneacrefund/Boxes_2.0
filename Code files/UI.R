3bbf2ccb9709dfba88ae73d0dad0dbda2572b36d
Colin Custer <colin.custer@oneacrefund.org>
2016-03-29 19:04:57 +0300

    Initial Shiny App commit

---
+++ Code files/UI.R
@@ -0,0 +1,11 @@
+# Boxes 2.0: UI script
+# Author: Colin Custer (colin.custer@oneacrefund.org)
+# Description: UI tool for geospatial Shiny app
+# Date created: 29 Mar 2016
+# Date modified: 29 Mar 2016
+
+ui <- fluidPage(
+    
+    
+    
+)
---
+++ Code files/server.R
@@ -0,0 +1,7 @@
+# Boxes 2.0: Server script
+# Author: Colin Custer (colin.custer@oneacrefund.org)
+# Description: Back-end server script for Shiny app
+# Date created: 29 Mar 2016
+# Date modified: 29 Mar 2016
+
+server <- function (input, output) {}
