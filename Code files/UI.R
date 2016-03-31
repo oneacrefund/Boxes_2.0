da9136656535bf05711446cb803c227e0f0142f7
Colin Custer <colin.custer@oneacrefund.org>
2016-03-31 22:13:21 +0300

    Display base layer in Shiny app

--- Code files/UI.R
+++ Code files/UI.R
@@ -2,10 +2,29 @@
 # Author: Colin Custer (colin.custer@oneacrefund.org)
 # Description: UI tool for geospatial Shiny app
 # Date created: 29 Mar 2016
-# Date modified: 29 Mar 2016
+# Date modified: 30 Mar 2016
 
-ui <- fluidPage(
+## Note, while under construction this lives in "Code Files" subdirectory. 
+## it should be moved to a Shiny directory when finished. 
+
+shinyUI(fluidPage(
+    titlePanel("Filters"),
     
-    
-    
-)
+    sidebarLayout(
+        sidebarPanel(
+            helpText("Set filter values"),
+            
+            selectInput("base", 
+                        label = "Choose a base layer to display",
+                        choices = c("Population", "Land Size", "Urban Areas", 
+                                    "Landcover", "water"), 
+                        selected = "Land Size")
+            
+            # sliderInput("range", 
+            #             label = "Range of interest:",
+            #             min = 0, max = 100, value = c(0, 100))
+        ),
+        
+        mainPanel(leafletOutput("map"))
+    )
+))
--- Code files/server.R
+++ Code files/server.R
@@ -2,6 +2,61 @@
 # Author: Colin Custer (colin.custer@oneacrefund.org)
 # Description: Back-end server script for Shiny app
 # Date created: 29 Mar 2016
-# Date modified: 29 Mar 2016
+# Date modified: 30 Mar 2016
 
-server <- function (input, output) {}
+## Note, while under construction this lives in "Code Files" subdirectory. 
+## it should be moved to a Shiny directory when finished. 
+
+#### Set up ####
+## directories ## 
+wd <- "~/drive/Boxes_2.0/Shiny"
+cd <- paste(wd, "Code files", sep = "/")
+dd <- paste(wd, "data", sep = "/")
+
+## libraries ##
+libs <- c("rgdal", "tidyr", "rgeos", "raster", "tiff", "ggplot2", "leaflet",
+          "dplyr", "spatial.tools", "shiny", "shinythemes", "shinyBS")
+lapply(libs, require, character.only = TRUE)
+rm(libs)
+cat("\014")
+
+## source data set-up (loads latest data) ##
+source("data_prep.r")
+
+#### functions ####
+
+#### server logic ####
+
+shinyServer(
+    function(input, output) {
+        output$map <- renderLeaflet({
+           
+            # Create a Progress object
+            progress <- shiny::Progress$new()
+            # Make sure it closes when we exit this reactive, even if there's an error
+            on.exit(progress$close())
+            
+            progress$set(message = "Drawing Map", value = 0)
+            
+            data <- switch(input$base,
+                           "Population" = pop,
+                           "Land Size" = av.size,
+                           "Urban Areas" = rpu.des,
+                           "Landcover" = lc, 
+                           "water" = wm)
+            
+            color <- switch(input$base,
+                            "Population" = colorNumeric("YlOrRd", domain = c(0, 500)),
+                            "Land Size" = colorNumeric("Greens", domain = c(0, 500)),
+                            "Urban Areas" = c("darkgreen", "yellow", "red"),
+                            "Landcover" = "YlGn",
+                            "water" = c("transparent", "darkblue"))
+
+            leaflet() %>%
+                addRasterImage(data, color,
+                               project = FALSE, maxBytes = Inf)
+            
+        })
+    }
+)
+
