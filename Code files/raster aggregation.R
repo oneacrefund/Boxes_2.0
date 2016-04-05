bc22b6d4b7f6e8056ed86a96f09189b324909459
Colin Custer <colin.custer@oneacrefund.org>
2016-04-05 12:26:30 +0300

    Raster Agg: initial commit

---
+++ Code files/raster aggregation.R
@@ -0,0 +1,12 @@
+# raster aggregation and value vector 
+# created by: colin custer (colin.custer@oneacrefund.org)
+# created for: Boxes 2.0 (https://github.com/ccc5vd/Boxes_2.0)
+# date created: 5 Apr 2016
+# date modified: 5 Apr 2016
+# description: aggregate all data rasters to specificed resolutions
+#              then return a matrix of values for each resolution. In this
+#              matrix, each row is a layer, each column a cell. Then for each 
+#              cell, you will be able to return a numeric vector of values for 
+#              each layer, which can be evaluated based on filter criteria
+
+
