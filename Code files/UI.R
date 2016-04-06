1cc41faa7477a876ed561f04c1f954e000713c60
Colin Custer <colin.custer@oneacrefund.org>
2016-04-06 11:50:39 +0300

    add res slider

--- Code files/UI.R
+++ Code files/UI.R
@@ -2,7 +2,7 @@
 # Author: Colin Custer (colin.custer@oneacrefund.org)
 # Description: UI tool for geospatial Shiny app
 # Date created: 29 Mar 2016
-# Date modified: 4 Apr 2016
+# Date modified: 6 Apr 2016
 
 ## Note, while under construction this lives in "Code Files" subdirectory. 
 ## it should be moved to a Shiny directory when finished. 
@@ -12,6 +12,9 @@ shinyUI(fluidPage(
     
     sidebarLayout(
       sidebarPanel(
+          sliderInput("res", label = "Resolution (sqkm)", value = 10, 
+                       min = 0, step = 5, max = 500, round = 2, post = "km"), # TODO: make rounding to nearest 5 work
+          
         selectInput("base",
           label = h5("Choose a base layer to display"),
               choices = c("Default", "Population", "Land Size", "Urban Areas",
